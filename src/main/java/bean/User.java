d51e0c22b2b637ab04e12b3995ab5a3b56d843ea
BoumouzounaBrahimVall <brahimvall.boumouzouna@etu.fstm.ac.ma>
2023-04-02 22:49:43 +0000

    added date of birth

--- src/main/java/bean/User.java
+++ src/main/java/bean/User.java
@@ -3,6 +3,11 @@ package bean;
 import dao.Account;
 import dao.DAOAccount;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import java.util.Date;
 
 //import javax.faces.bean.ManagedBean;
@@ -82,7 +87,42 @@ public class User {
 
         }
 
-        return "index";
+        return "register";
+    }
+    public void validateDateOfBirth(FacesContext context, UIComponent component, Object value) throws ValidatorException{
+        Date dateOfBirth = (Date) value;
+        System.out.println(value);
+        if (dateOfBirth == null) {
+            FacesMessage message = new FacesMessage("date incorrecte");
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(message);
+        }
+    }
+    private String mno;
+
+    public String getMno() {
+        return mno;
     }
 
+    public void setMno(String mno) {
+        this.mno = mno;
+    }
+
+    public void validateModelNo(FacesContext context, UIComponent comp,
+                                Object value) {
+
+        System.out.println("inside validate method");
+
+        String date = (String) value;
+
+        if (date.length() < 4) {
+            ((UIInput) comp).setValid(false);
+
+            FacesMessage message = new FacesMessage(
+                    "Minimum length of model number is 4");
+            context.addMessage(comp.getClientId(context), message);
+
+        }
+
+    }
 }
